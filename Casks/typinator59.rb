4c5d0e4c1884017c58f29b55897581a56c3ccf34
Louis Romero <lpromero@google.com>
2017-05-31 16:14:39 +0200

    Update URL for download.

--- Casks/typinator59.rb
+++ Casks/typinator59.rb
@@ -2,10 +2,9 @@ cask 'typinator59' do
   version '5.9'
   sha256 '7513b11ba2f224a3ad2dd05a425e44b57a584d9d0a6ee1b30bc6f4a05ee7618d'
 
-  url "http://www.ergonis.com/downloads/products/typinator/Typinator#{version.no_dots}-Install.dmg",
-      user_agent: :fake
+  url "http://www.ergonis.com/downloads/archive/typinator/Typinator59-Install.dmg"
   name 'Typinator'
-  homepage 'http://www.ergonis.com/'
+  homepage 'http://www.ergonis.com/products/typinator'
 
   accessibility_access true
 
